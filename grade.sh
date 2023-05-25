cfe3d5bbe3277504d391ce177fd53539f123535a
Jkorah0 <130091708+Jkorah0@users.noreply.github.com>
2023-05-25 11:12:38 -0700

    updating to github

--- grade.sh
+++ grade.sh
@@ -14,3 +14,32 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+if [[ -f $1 ]] && [[ $1 != "ListExamples.java" ]]
+then
+echo "Wrong file submitted."
+exit
+else
+echo "Right File Submitted"
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+cd grading-area
+javac -cp ".;lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java"
+if [[ $? == 1 ]]
+then
+echo "Compile Error in Submission"
+exit
+else
+java -cp ".;lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples"
+status=$?
+if [[ $status == 0 ]]
+then
+echo "Test Passed"
+elif [[ $status == 1 ]]
+then
+echo "Test Failed"
+else
+echo "Exit Code"
+fi
+fi
+fi
